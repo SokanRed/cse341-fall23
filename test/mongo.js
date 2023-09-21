46cd0c3d1dfa383907e819caeb57d8024320970f
Denis PC Poussard <denis@sokan.fr>
2023-09-21 22:41:28 +0200

    created lesson2 team and test

---
+++ test/mongo.js
@@ -0,0 +1,9 @@
+const { MongoClient } = require('mongodb');
+const uri = "mongodb+srv://dernakos:Sp3gno24Lini@cluster0.cpiccp7.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri);
+try {
+    client.connect()
+
+} catch (error) {
+
+};
